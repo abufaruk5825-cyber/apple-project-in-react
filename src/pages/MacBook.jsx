aa71e306eea32d377a4ac99510f3b9f9612f783a
Seya Derege <abuzubeir1688@gmail.com>
2026-04-19 00:23:33 +0300

    Enhance Apple website layout and styling for better UI

--- src/pages/MacBook.jsx
+++ src/pages/MacBook.jsx
@@ -6,8 +6,8 @@ function MacBook() {
   return (
     <div className="detail-page">
       <button className="back-btn" onClick={() => navigate(-1)}>← Back</button>
-      <div className="detail-hero" style={{ backgroundImage: `url(${macbook})`,margin:"0 10px 0 10px"  }}>
-        <h1>MacBook Pro</h1>
+      <div className="detail-hero" style={{ backgroundImage: `url(${macbook})`,margin:"0 10px 10px 10px" }}>
+       <h1 style={{ color: "rgba(191, 111, 26, 1)" }}>MacBook Pro</h1>
         <p>A work of smart</p>
       </div>
       <div className="detail-content">
